99eca73a0672934f7efb0a55da20969c27fbb060
David Nault <dnault@mac.com>
2015-09-06 19:34:08 -0700

    remove unised imports

--- src/main/java/com/github/dnault/therapi/runtimejavadoc/Comment.java
+++ src/main/java/com/github/dnault/therapi/runtimejavadoc/Comment.java
@@ -1,12 +1,7 @@
 package com.github.dnault.therapi.runtimejavadoc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
 
-import java.io.IOException;
 import java.util.List;
 
 import static com.github.dnault.therapi.runtimejavadoc.internal.RuntimeJavadocHelper.unmodifiableDefensiveCopy;
