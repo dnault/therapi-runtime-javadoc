bebe2b5f8675b28ab7c0a2f8578a51bb07a2f906
David Nault <dnault@mac.com>
2015-10-11 09:41:21 -0700

    Add comment formatter

---
+++ src/test/java/com/github/therapi/runtimejavadoc/CommentFormatterTest.java
@@ -0,0 +1,34 @@
+package com.github.therapi.runtimejavadoc;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class CommentFormatterTest {
+    private final CommentFormatter formatter = new CommentFormatter();
+
+    @Test
+    public void formatterHandlesAllCommentElements() {
+        Comment c = new Comment(Arrays.asList(
+                new CommentText("before "),
+                new InlineLink(new Link("label", "className", "memberName")),
+                new CommentText(" "),
+                new InlineTag("code", "List<String>"),
+                new CommentText(" "),
+                new InlineTag("literal", "<>&\""),
+                new InlineTag("unrecognized", "foo"),
+                new CommentText(" "),
+                new CommentElement() {
+                    @Override public String toString() {
+                        return "unexpected element";
+                    }
+                },
+                new CommentText(" after")
+        ));
+
+        String expected = "before {@link className#memberName label} <code>List&lt;String&gt;</code> &lt;&gt;&amp;&quot;{@unrecognized foo} unexpected element after";
+        assertEquals(expected, formatter.format(c));
+    }
+}
