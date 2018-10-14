212204e7003037c51d7912a68d74f7665becf9a6
bbottema <b.bottema@projectnibble.org>
2018-10-14 23:13:27 +0200

    #32: Implemented SeeAlsoJavadoc for Methods and fixed SeeAlsoJavadoc to conform to the Javadoc rules (instead of only a link, it can also be a string literal or HTML link)

--- therapi-runtime-javadoc/src/main/java/com/github/therapi/runtimejavadoc/internal/parser/CommentParser.java
+++ therapi-runtime-javadoc/src/main/java/com/github/therapi/runtimejavadoc/internal/parser/CommentParser.java
@@ -10,22 +10,17 @@ import com.github.therapi.runtimejavadoc.Link;
 import com.github.therapi.runtimejavadoc.Value;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.github.therapi.runtimejavadoc.internal.RuntimeJavadocHelper.isBlank;
-import static java.util.Arrays.asList;
 import static java.util.regex.Pattern.compile;
 
 class CommentParser {
 
     private static final Pattern inlineTag = compile("\\{@(\\w+)(?:\\s+([\\w#][^}]+)?)?}");
 	
-	// https://regex101.com/r/3DoNfK/2
-	private static final Pattern linkPattern = compile("^(?<classname>[\\w.]+)?(?:#(?<member>\\w+))?(?:\\((?<params>.*)\\))?(?:\\s(?<label>.+))?$");
-    
     // https://regex101.com/r/KhEo62/4
     private static final Pattern valuePattern = compile("^(?:(?<classname>[\\w.]+)#)?#?(?<member>\\w+)$");
 
@@ -80,24 +75,10 @@ class CommentParser {
     }
 
     private static InlineLink createLinkElement(String owningClass, String value) {
-        Matcher linkMatcher = linkPattern.matcher(value);
-        if (!linkMatcher.matches()) {
+        Link javadocLink = LinkParser.createLinkElement(owningClass, value);
+        if (javadocLink == null) {
             throw new AssertionError("Link didn't match regex format");
         }
-        String classRef = linkMatcher.group("classname");
-        String memberRef = linkMatcher.group("member");
-        String params = linkMatcher.group("params");
-        String label = linkMatcher.group("label");
-    
-        String effectiveClassName = classRef == null ? owningClass : classRef;
-        String effectiveLabel = label != null ? label : linkMatcher.group(0);
-        return new InlineLink(new Link(effectiveLabel, effectiveClassName, memberRef, formatMember(params)));
-    }
-    
-    private static List<String> formatMember(String params) {
-		if (params != null && !params.trim().isEmpty()) {
-			return asList(params.trim().split(",\\s*"));
-		}
-		return Collections.emptyList();
+        return new InlineLink(javadocLink);
     }
 }
---
+++ therapi-runtime-javadoc/src/main/java/com/github/therapi/runtimejavadoc/internal/parser/SeeAlsoParser.java
@@ -0,0 +1,48 @@
+package com.github.therapi.runtimejavadoc.internal.parser;
+
+import com.github.therapi.runtimejavadoc.Link;
+import com.github.therapi.runtimejavadoc.SeeAlsoJavadoc;
+import com.github.therapi.runtimejavadoc.SeeAlsoJavadoc.HtmlLink;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
+
+public class SeeAlsoParser {
+	
+	private static final Pattern stringLiteralPattern = compile("^\".*\"$");
+	private static final Pattern htmlLink = compile("(?s)<a\\s*href=['\"](?<link>.+?)['\"]\\s*>(?<text>.+)<\\/a>");
+	
+	public static SeeAlsoJavadoc parseSeeAlso(String owningClass, String value) {
+		SeeAlsoJavadoc seeAlsoJavadoc = parseAsStringLiteral(value);
+		if (seeAlsoJavadoc == null) {
+			seeAlsoJavadoc = parseAsHtmlLink(value);
+		}
+		if (seeAlsoJavadoc == null) {
+			seeAlsoJavadoc = parseAsJavadocLink(owningClass, value);
+		}
+		if (seeAlsoJavadoc == null) {
+			throw new AssertionError("SeeAlso not recognized as string literal, HTML link or Javadoc link");
+		}
+		
+		return seeAlsoJavadoc;
+	}
+	
+	private static SeeAlsoJavadoc parseAsStringLiteral(String value) {
+		return stringLiteralPattern.matcher(value).matches() ? new SeeAlsoJavadoc(value) : null;
+	}
+	
+	private static SeeAlsoJavadoc parseAsHtmlLink(String value) {
+		Matcher matcher = htmlLink.matcher(value);
+		if (matcher.matches()) {
+			return new SeeAlsoJavadoc(new HtmlLink(matcher.group("text"), matcher.group("link")));
+		}
+		return null;
+	}
+	
+	private static SeeAlsoJavadoc parseAsJavadocLink(String owningClass, String value) {
+		Link javadocLink = LinkParser.createLinkElement(owningClass, value);
+		return javadocLink == null ? null : new SeeAlsoJavadoc(javadocLink);
+	}
+}
